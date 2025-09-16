b88a3df5ea591a9275a15d461fee5fdbad529dcb
AWS ParallelCluster user <ec2-user@ip-10-0-11-180.ap-southeast-1.compute.internal>
2025-09-16 15:13:23 +0000

    nano

---
+++ unity.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+#SBATCH --time=24:00:00   # walltime
+
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --cpus-per-task=1   # number of CPUs for this task
+#SBATCH -J "unity"   # job name
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o unity.%N.%j.out # STDOUT
+#SBATCH -e unity.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+python -u -c "import PyHipp as pyh; \
+import DataProcessingTools as DPT; \
+import os; \
+import time; \
+t0 = time.time(); \
+DPT.objects.processDirs(dirs=None, objtype=pyh.Unity, saveLevel=1); \
+print(time.time()-t0);"
+
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:269423819877:awsnotify --message "UnityJobDone"
+
